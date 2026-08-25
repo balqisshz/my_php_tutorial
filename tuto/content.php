dba272f0375291315c277a1404e02a2febcf6f5e
balqisshz <puteribalqis.sharunezam@student.gmi.edu.my>
2026-08-26 06:25:06 +0800

    content from tuto 15

---
+++ tuto/content.php
@@ -0,0 +1,7 @@
+<div>
+    <h1><?php echo 'some  content!!!' ?></h1>
+</div>
+
+<!-- so if we want to add this content to several different pages in the html template -->
+
+<!-- update once je kt sini-->
