a24020e544f78977c2f510fbcea515031d12358a
balqisshz <puteribalqis.sharunezam@student.gmi.edu.my>
2026-08-26 06:29:50 +0800

    Index PHP Pizza

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+
+ <!-- include file header n footer ke sini-->
+  <?php include('template/header.php'); ?>  
+
+  <?php include('template/footer.php'); ?>
+  
+
+</html>
