97f52502206e9b88f94a9f9f041e378305631f47
balqisshz <puteribalqis.sharunezam@student.gmi.edu.my>
2026-08-26 06:14:43 +0800

    Tuto 5- Numbers

---
+++ tuto/tuto6.php
@@ -0,0 +1,92 @@
+<?php
+
+// Another data type which is integer and float
+// integer tu whole numbers
+// floats tu contain decimals
+
+$radius = 25;   // ni integer
+$pi = 3.14;     // ni floats
+
+
+// ni basic operator    *, /, +, -, **
+// * ni multiply
+// / ni divide
+// + ni plus
+// - ni minus
+// ** ni find the power of
+
+//echo $pi * $radius**2;
+
+
+
+
+// order of operation (B I D M A S) bila buat calculation, kita in in order BIDMAS ni
+// kene execute apa yg dlm bracket first (B)
+// if ada any indices which is power of comes to second (I)
+// division comes third (D)
+// multiplication comes fourth
+// then addition (A)
+// then subtraction (S)
+ // cth:
+//echo 2 * (4 + 9) / 3;
+
+
+
+
+// increment & decrement operators
+// this is a way to add values to numbers or minus values for number
+// forexmaple nk add radius
+// echo $radius++
+// double pluss is an increment operator and it just adds one to the radius
+// kita akan dpt 25 bile run sbb tdi tu yg echo sekian ii tu just echoing value die je before do the operation
+// so bile nk add jdi 26, buat echo $radius; mcm yg minus kt bwh ni
+
+//echo $radius--;
+//echo $radius;
+
+
+
+
+// shorthand operators
+
+
+$age = 20;   // first tulis new variable
+
+// ni cara short
+//$age -= 10;
+//echo $age;
+// so akan tolak 10
+
+
+//$age *= 2;  //ni multiply
+//echo $age;
+
+
+
+
+//number functions
+
+
+//echo floor($pi);  // fuction floor ni akan take in a number or a float
+// and it flows that to the nearest integer
+
+//echo ceil($pi);  // fucntion ceil ni take the values and move it up to the nearest integer
+// means mcm tamh ah
+
+
+//echo pi();  //function ni get values for pi
+// tpi bkn yg kita tulis, dia akan bgi yg longer version punya pi
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+  <title>PHP Tutorials</title>
+</head>
+<body>
+    
+
+
+</body>
+</html>
